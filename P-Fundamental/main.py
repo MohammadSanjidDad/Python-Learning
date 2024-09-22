906b8cc43747286ba1b7526ee3ae3d276b957632
Mohammad Sanjid Dad <m.sanjiddad@gmail.com>
2024-09-22 16:58:01 +0600

    First Code

--- P-Fundamental/main.py
+++ P-Fundamental/main.py
@@ -1,3 +1,20 @@
-# Press the green button in the gutter to run the script.
+import datetime
+
+""" TODO: Multiline Comment
+     Create a Function that take 2 Input From User. First User name and Second User age. Then Find the User Date-Of-Birth Year.
+"""
+
+
+def user():
+    """ Function take input from user and return user Date-Of-Birth. """
+    user_name = str(input("Enter Username: "))
+    user_age = int(input("Enter user age: "))
+    # Operations.
+    x = datetime.datetime.now().year
+    a = x - user_age
+    return a
+
+
+# TODO: Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    pass
+    print("User Date-Of-Birth", user())
